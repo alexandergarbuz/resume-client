bb7aebb4957cca48b2c3d553da201977b3161a5e
alexandergarbuz <alexander.garbuz@gmail.com>
2024-06-26 17:01:55 -0500

    Updated Avatar component

--- src/component/Avatar.js
+++ src/component/Avatar.js
@@ -4,23 +4,20 @@ import React  from "react";
 
 function Avatar(props) {
 
-    const id = props.id;
-    const src = props.src;
-    const size = props.size;
-    const link = props.link;
-    const header1 = props.header1;
-    const header2 = props.header2;
-    const header3 = props.header3;
+    const { id, src, size, link, header1, header2, header3 } = props;
 
     return (
-        <div id={id} style={{textAlign:'center'}}>
-            <img src={src} width={size} height={size} alt={header1} style={{borderRadius:'50%'}} />
-            <p>
-                <a href={link} target="_blank"><strong>{header1}</strong></a><br/>
+        <div  id={'avatarImageBlock' + id} style={{textAlign:'center'}}>
+            <div>
+                <a id={'avatarImageLink' + id} href={link} target="_blank">
+                    <img id={'avatarImage' + id} src={src} width={size} height={size} alt={header1} title={header1} style={{borderRadius:'50%'}} />
+                </a>
+            </div>
+            <div>
+                <a id={'avatarHeaderLink' + id} href={link} target="_blank"><strong>{header1}</strong></a><br/>
                 <i>{header2}</i><br/>
                 {header3}
-            </p>
-
+            </div>
         </div>
     );
 }
