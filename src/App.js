48bba29635f386964dcbc7b101ec238f8aeaf6cb
alexandergarbuz <alexander.garbuz@gmail.com>
2024-06-27 10:40:44 -0500

    Added WorkHistory component

--- src/App.js
+++ src/App.js
@@ -8,15 +8,16 @@ import Footer from './Footer';
 
 import SkillList from './SkillList';
 import Recommendations from './Recommendations';
+import WorkHistory from './component/WorkHistory';
 
 
 function App() {
   return (
-    <div className='w3-content'>
+    <div className="container">
       <ContactInformation />
       <SkillList apiUrl="https://resume-service.aws.garbuz.com/api/resume/skills/1" />
+      <WorkHistory  apiUrl="https://resume-service.aws.garbuz.com/api/resume/show/1"/>
       <Recommendations apiUrl="https://resume-service.aws.garbuz.com/api/resume/show/1" imageBaseUrl="https://resume-service.aws.garbuz.com/img/"/>
-      <WorkExperience/>
       <Certifications/>
       <Education/>
       <Footer/>
---
+++ src/component/WorkHistory.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react';
+import Accordion from 'react-bootstrap/Accordion';
+
+function parseDate(datToParse) {
+    return (datToParse.getMonth() + 1) + '/' + datToParse.getFullYear();
+}
+
+function WorkHistory(props) {
+    const [data, setData] = useState([]);
+    const [isLoading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch(props.apiUrl)
+            .then(response => {
+                if(! response.ok) {
+                    throw new Error("Cannot load data");
+                }
+                return response.json();
+            })
+            .then(data => {
+                const loadedJobs = data.jobs.map(job => {
+                const startDate = new Date(job.start);
+                const endDate = new Date(job.end);
+                const formattedStartDate = parseDate(startDate);
+                const formattedEndDate = parseDate(endDate);;
+
+                    return (
+                        <Accordion.Item eventKey={job.id.toString()} key={job.id}>
+                            <Accordion.Header>{job.title} @ {job.companyName} ({formattedStartDate} - {formattedEndDate})</Accordion.Header>
+                            <Accordion.Body>
+                                <ul>
+                                    {job.responsibilities.map(r => (
+                                        <li key={r.id}>{r.text}</li>
+                                    ))}
+                                </ul>
+                            </Accordion.Body>
+                        </Accordion.Item>
+                    );
+                });
+                setData(loadedJobs);
+                setLoading(false);
+            })
+        .catch(error => {
+                console.error('Error fetching data:', error);
+                setLoading(false);
+        });
+    }, [props.apiUrl]);
+
+    if (isLoading) {
+        return (
+            <div id="workHistory">
+                <h2>Work History</h2>
+                <p>Loading data....</p>
+                <hr />
+            </div>
+        );
+    }
+
+    return (
+        <div id="workHistory">
+            <h2>Work History</h2>
+            <Accordion>
+                {data}
+            </Accordion>
+        </div>
+    );
+}
+
+export default WorkHistory;
